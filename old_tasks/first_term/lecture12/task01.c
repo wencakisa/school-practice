29e68b93f3a8fd5adf25a9d2d547f1e07d1b92b4
wencakisa <wencakisa@gmail.com>
2017-02-10 11:18:12 +0200

    Simplified integer reversing

--- old_tasks/first_term/lecture12/task01.c
+++ old_tasks/first_term/lecture12/task01.c
@@ -1,12 +1,7 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 
 size_t get_len(int);
-char int_to_char(int);
-int char_to_int(char);
 int get_multiplier(size_t);
-int str_to_int(char*);
 int reverse(int);
 
 int main() {
@@ -15,28 +10,17 @@ int main() {
     printf("Enter a number: ");
     scanf("%d", &n);
 
-    printf("Result: %d\n", reverse(n));
+    printf("Reversed: %d\n", reverse(n));
 
     return 0;
 }
 
 size_t get_len(int n) {
     size_t len = 0;
-
-    while(n > 0) {
-        len++;
-        n /= 10;
-    }
+    
+    for(; n > 0; len++, n /= 10);
 
     return len;
-}
-
-char int_to_char(int n) {
-    return n + '0';
-}
-
-int char_to_int(char c) {
-    return c - '0';
 }
 
 int get_multiplier(size_t len) {
@@ -47,22 +31,17 @@ int get_multiplier(size_t len) {
     return multiplier;
 }
 
-int str_to_int(char *str) {
+int reverse(int n) {
     int result = 0;
-    size_t len = strlen(str);
+    size_t len = get_len(n);
+    int multiplier = get_multiplier(len);
     
-    int multiplier = get_multiplier(len);    
-    for(size_t i = 0; i < len; i++, multiplier /= 10)
-        result += char_to_int(str[i]) * multiplier;
+    for(size_t i = 0; i < len; i++) {
+        result += (n % 10) * multiplier;
+
+        multiplier /= 10;
+        n /= 10;
+    }
 
     return result;
-}
-
-int reverse(int n) {
-    char *n_string = malloc(get_len(n));
-    
-    for(size_t i = 0; n > 0; i++, n /= 10)
-        n_string[i] = int_to_char(n % 10);
-    
-    return str_to_int(n_string) * 3;
 }

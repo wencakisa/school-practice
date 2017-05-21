3557a17540ec2160274d922704b18f0f43075d60
wencakisa <wencakisa@gmail.com>
2017-05-21 15:05:24 +0300

    Removed useless string.h

--- second_term/dynamic_memory/stack_and_rpn/main.c
+++ second_term/dynamic_memory/stack_and_rpn/main.c
@@ -1,12 +1,7 @@
-#include <string.h>
-
 #include "calculator.h"
 
-#define SIZE 50
-
 int main() {
-    char rpn_exp[SIZE] = "5 1 2 + 4 * + 3 -";
-    rpn_calculator_t calculator = init_calculator(rpn_exp);
+    rpn_calculator_t calculator = init_calculator("5 1 2 + 4 * 3 - +");
     print_result(calculator);
 
     set_expression(&calculator, "6   5     3  + 2 4 ^ - +");
